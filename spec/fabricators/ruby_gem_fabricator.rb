3bfe4955900607c7886946a0d56b0053ca06af87
Michael Mentele <Michaelrmentele@gmail.com>
2016-12-20 15:12:31 -0800

    updated schema

--- spec/fabricators/ruby_gem_fabricator.rb
+++ spec/fabricators/ruby_gem_fabricator.rb
@@ -1,5 +1,7 @@
 Fabricator(:ruby_gem) do
-  url { Faker::Internet.url }
-  downloads { Faker::Number.between(1, 50000)}
-  name { Faker::Name.name }
+  url       { Faker::Internet.url }
+  downloads { Faker::Number.between(1, 5000000) }
+  name      { Faker::Name.name }
+  stars     { Faker::Number.between(1, 50000) }
+  description { Faker::Lorem.paragraph(10) }
 end
