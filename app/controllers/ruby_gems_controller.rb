c7c0749aca9c621c6c182237a7b71e29f1e89a27
Michael Mentele <Michaelrmentele@gmail.com>
2016-12-20 12:25:57 -0800

    basic search and ranking

--- app/controllers/ruby_gems_controller.rb
+++ app/controllers/ruby_gems_controller.rb
@@ -1,5 +1,16 @@
 class RubyGemsController < ApplicationController
   def index
-    @gems = RubyGem.all
+    @gems = RubyGem.order(downloads: :desc)
+  end
+
+  def search
+    term = params[:search]
+    if term != ""
+      @gems = RubyGem.where('name LIKE ?', "%#{term}%")
+      render :index
+    else
+      flash[:error] = "If you don't know how to search... should you be looking?"
+      redirect_to ruby_gems_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :ruby_gems, only: [:index]
+  get "ruby_gems/search", to: "ruby_gems#search"
 
   get "*path", to: "ruby_gems#index"
 end
