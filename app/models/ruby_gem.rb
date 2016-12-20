9a7a66c491514cdf08c07661b333176c0f527bfe
Michael Mentele <Michaelrmentele@gmail.com>
2016-12-20 14:50:23 -0800

    initial elastic search

--- app/models/ruby_gem.rb
+++ app/models/ruby_gem.rb
@@ -1,2 +1,26 @@
 class RubyGem < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks # auto imports to active record to keep them synchronized
+
+  index_name ["ruby_gem", Rails.env].join('_') # create separate indexes for each environment
+
+  def self.search(query, options={})
+    search_definition = {
+      query: {
+        multi_match: {
+          query: query,
+          fields: ["name^100", "description^50"],
+          operator: "and"
+        }
+      }
+    }
+
+    __elasticsearch__.search(search_definition)
+  end
+
+  def as_indexed_json(options={})
+    as_json(
+      only: [:name, :description],
+    )
+  end
 end
