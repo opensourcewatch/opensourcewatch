c6b9bb69c94a419ec19f4bc4a56f82c7f2743f6d
Lukas <Lukas.Nimmo@gmail.com>
2016-12-21 12:18:42 -0600

    Refactored Scraper. Fixed a few parsing mistakes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-50.times do
-  Fabricate(:ruby_gem)
-end
+# 50.times do
+#   Fabricate(:ruby_gem)
+# end
--- scraper.rb
+++ scraper.rb
@@ -1,69 +1,110 @@
 require 'nokogiri'
 require 'open-uri'
 
-def scrape_gem_meta_data(gems, base_url, doc)
-  # scrape per page of results
-  doc.css('a.gems__gem').each do |gem_link|
-    gem_url = base_url + gem_link['href'] # /gems/gem_name
-    gem_doc = Nokogiri::HTML(open(gem_url))
+# Scraper for Ruby Gem data from RubyGems.org
+class RubyGemsScraper
+  @ruby_gems_base_url = 'https://rubygems.org'
+  @curr_gem_doc = nil
 
-    gem_name = gem_doc.css('h1 a')[0].text.strip
-    gem_downloads = gem_doc.css('.gem__downloads')[0].text.strip
-    github_url = ''
-    home = gem_doc.css('#home')
-    source = gem_doc.css('#code')
+  class << self
+    # Iterates through entire alphabet of pagination of rubygems.org's gems 
+    # and sends each page to check_ruby_gems_org to update/create gems/gem data
+    def check_for_new_gems
+      alphabet = ('A'..'Z').to_a
+      current_letter_path = "/gems?letter="
 
-    if home.any? && home[0]['href'].include?('github')
-      github_url = home[0]['href']
-    elsif source.any? && source[0]['href'].include?('github')
-      github_url = source[0]['href']
+      alphabet.each do |letter|
+        page_num = 1
+        loop do
+          doc = Nokogiri::HTML(open(@ruby_gems_base_url + current_letter_path + letter + "&page=#{page_num}"))
+          check_ruby_gems_org(doc)
+
+          page_num += 1
+          break if doc.css('.next_page.disabled').any?
+        end
+      end
     end
 
-    if github_url.length > 0
-      unless RubyGem.exists?(name: gem_name)
-        gem = RubyGem.create(name: gem_name, url: github_url, downloads: gem_downloads)
+    private
+
+    # Finds all gems on a given page and checks for new gems and updates
+    # existing ones with the following data:
+    # - name
+    # - url (github)
+    # - downloads
+    #
+    # doc: Nokogiri HTML document of a pagination result of rubygems.org
+    #
+    def check_ruby_gems_org(doc)
+      doc.css('a.gems__gem').each do |gem_link|
+        @curr_gem_doc = Nokogiri::HTML(open(@ruby_gems_base_url + gem_link['href'])) # /gems/gem_name
+
+        if github_url.length > 0
+          if RubyGem.exists?(name: gem_name)
+            RubyGem.update(name: gem_name, url: github_url, downloads: gem_downloads)
+          else
+            RubyGem.create(name: gem_name, url: github_url, downloads: gem_downloads)
+          end
+        end
+      end
+    end
+
+    def gem_name
+      @curr_gem_doc.css('h1 a')[0].text.strip
+    end
+
+    def gem_downloads
+      @curr_gem_doc.css('.gem__downloads')[0].text.strip.gsub(',', '')
+    end
+
+    def github_url
+      home = @curr_gem_doc.css('#home')
+      source = @curr_gem_doc.css('#code')
+
+      if home.any? && home[0]['href'].include?('github')
+        home[0]['href']
+      elsif source.any? && source[0]['href'].include?('github')
+        source[0]['href']
+      else
+        ''
       end
     end
   end
 end
 
-# Gets the following:
-# - raw README.md file
-# - number of stars the project has
-#
-# Example project's Github url vs raw url
-# - Github: https://github.com/rspec/rspec/blob/master/README.md
-# - Raw: https://raw.githubusercontent.com/rspec/rspec/master/README.md
-def scrape_gem_github_data
-  ex_github_url = 'https://github.com/rails/rails'
-  github_doc = Nokogiri::HTML(open(ex_github_url))
+# Scrapes data for Gems and Users on Github.com
+class GithubScraper
+  class << self
+    # Gets the following:
+    # - number of stars the project has
+    # - raw README.md file
+    #
+    # Example project's Github url vs raw url
+    # - Github: https://github.com/rspec/rspec/blob/master/README.md
+    # - Raw: https://raw.githubusercontent.com/rspec/rspec/master/README.md
+    def update_gem_github_data
+      RubyGem.all.each do |gem|
+        github_doc = Nokogiri::HTML(open(gem.url))
+        stars = github_doc.css('ul.pagehead-actions li:nth-child(2) .social-count').to_s[/\d+\,\d+/]
 
-  RubyGem.all.each do |gem|
-    github_doc = Nokogiri::HTML(open(gem.url))
-    stars = github_doc.css('ul.pagehead-actions li:nth-child(2) .social-count').to_s[/\d+\,\d+/]
+        if github_doc.at('td span:contains("README")')
+          raw_file_url = gem.url.gsub('github', 'raw.githubusercontent') + '/master/README.md'
+          description = Nokogiri::HTML(open(raw_file_url)).css('body p').text
+        else
+          description = "Empty"
+        end
 
-    raw_url = ex_github_url.gsub('github', 'raw.githubusercontent') + '/master/README.md'
-    description = Nokogiri::HTML(open(raw_url)).css('body p').text
+        gem.update(stars: stars, description: description)
+      end
+    end
 
-    gem.update(stars: stars, description: description)
+    # Retrieves the top 100 contributors for each RubyGem
+    def all_gems_top_100_contributors
+
+    end
   end
 end
 
-base_url = 'https://rubygems.org'
-
-alphabet = ('A'..'Z').to_a
-
-category_url = "/gems?letter="
-alphabet.each do |letter|
-  page_num = 1
-  loop do
-    doc = Nokogiri::HTML(open(base_url + category_url + letter + "&page=#{page_num}"))
-    scrape_gem_meta_data(gems, base_url, doc)
-
-    page_num += 1
-    break if doc.css('.next_page .disabled').any?
-  end
-end
-
-scrape_gem_github_data
-update_score
+RubyGemsScraper.check_for_new_gems
+# GithubScraper.update_gem_github_data
+# RubyGem.update_score
