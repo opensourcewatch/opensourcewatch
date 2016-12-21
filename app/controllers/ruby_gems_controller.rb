3137122bc6e8ff1802fdb1cdbc0f67fe7472d7cd
Michael Mentele <Michaelrmentele@gmail.com>
2016-12-20 17:44:52 -0800

    elastic search and highlight snippets for ruby gems

--- app/controllers/ruby_gems_controller.rb
+++ app/controllers/ruby_gems_controller.rb
@@ -6,8 +6,32 @@ class RubyGemsController < ApplicationController
   def search
     term = params[:search]
     if term != ""
-      @gems = RubyGem.search(term).records.to_a
-      binding.pry
+      search = RubyGem.search(term)
+
+      results = search.results
+      gems = search.records.to_a
+
+      @search_results = []
+      gems.each_with_index do |gem, idx|
+        highlight = results[idx].highlight
+        if highlight.description
+          description = highlight.description[0]
+        else
+          description = gem.description
+        end
+
+        if highlight.name
+          name = highlight.name[0]
+        else
+          name = gem.name
+        end
+
+        @search_results << {
+          "description": description,
+          "name": name
+        }
+      end
+
       render :index
     else
       flash[:error] = "If you don't know how to search... should you be looking?"
