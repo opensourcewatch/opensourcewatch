a6040786d30b4c30661a22d748aebf7b5f23358b
Michael Mentele <Michaelrmentele@gmail.com>
2016-12-23 09:57:28 -0800

    unplugged elasticsearch

--- app/models/ruby_gem.rb
+++ app/models/ruby_gem.rb
@@ -1,8 +1,8 @@
 class RubyGem < ActiveRecord::Base
-  include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks # auto imports to active record to keep them synchronized
-
-  index_name ["ruby_gem", Rails.env].join('_') # create separate indexes for each environment
+  # include Elasticsearch::Model
+  # include Elasticsearch::Model::Callbacks # auto imports to active record to keep them synchronized
+  #
+  # index_name ["ruby_gem", Rails.env].join('_') # create separate indexes for each environment
 
   def self.update_score
     avg_downloads = self.average(:downloads).to_i
@@ -17,30 +17,30 @@ class RubyGem < ActiveRecord::Base
     end
   end
 
-  def self.search(query, options={})
-    search_definition = {
-      query: {
-        multi_match: {
-          fuzziness: 2,
-          query: query,
-          fields: ["name^100", "description^50"],
-          operator: "and"
-        }
-      },
-      highlight: {
-        fields: {
-          description: {},
-          name: {}
-        }
-      }
-    }
-
-    __elasticsearch__.search(search_definition)
-  end
-
-  def as_indexed_json(options={})
-    as_json(
-      only: [:name, :description],
-    )
-  end
+  # def self.search(query, options={})
+  #   search_definition = {
+  #     query: {
+  #       multi_match: {
+  #         fuzziness: 2,
+  #         query: query,
+  #         fields: ["name^100", "description^50"],
+  #         operator: "and"
+  #       }
+  #     },
+  #     highlight: {
+  #       fields: {
+  #         description: {},
+  #         name: {}
+  #       }
+  #     }
+  #   }
+  #
+  #   __elasticsearch__.search(search_definition)
+  # end
+  #
+  # def as_indexed_json(options={})
+  #   as_json(
+  #     only: [:name, :description],
+  #   )
+  # end
 end
