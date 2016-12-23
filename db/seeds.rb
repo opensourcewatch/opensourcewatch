6b113deef2de330a0b57152d8f507ce6592cba11
Lukas <Lukas.Nimmo@gmail.com>
2016-12-22 18:29:39 -0600

    Seeding option for RubyGem data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require_relative '../scraper.rb'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,6 +7,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# 50.times do
-#   Fabricate(:ruby_gem)
-# end
+RubyGemsScraper.upsert_gems(5)
