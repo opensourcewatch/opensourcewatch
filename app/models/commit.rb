5f2105dff8ca034629e345aab6a49993ae45f2f7
Michael Mentele <Michaelrmentele@gmail.com>
2016-12-27 14:41:53 -0800

    class method for retrieving commits as csv

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -2,4 +2,13 @@ class Commit < ActiveRecord::Base
   validates :github_identifier, uniqueness: true
 
   belongs_to :user
+
+  def self.to_csv
+    CSV.generate do |csv|
+      csv << column_names
+      all.each do |commit|
+        csv << commit.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
