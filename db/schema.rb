48721748d5681dfd1bbbaedb826e5b0f47073e98
Michael Mentele <Michaelrmentele@gmail.com>
2016-12-27 14:04:40 -0800

    created commits table

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161223223416) do
+ActiveRecord::Schema.define(version: 20161227215558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "commits", force: :cascade do |t|
+    t.string   "message"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ruby_gems", force: :cascade do |t|
     t.string   "url"
     t.string   "name"
     t.integer  "downloads"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.integer  "stars"
     t.float    "score"
     t.text     "description"
+    t.integer  "contributors"
   end
 
   create_table "users", force: :cascade do |t|
