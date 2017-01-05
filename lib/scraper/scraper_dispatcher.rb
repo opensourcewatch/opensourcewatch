68c76208a420285d117c7cb0d05f9d3c1ff859a1
Michael Mentele <Michaelrmentele@gmail.com>
2017-01-05 10:29:11 -0800

    added connection to redis queue

--- lib/scraper/scraper_dispatcher.rb
+++ lib/scraper/scraper_dispatcher.rb
@@ -1,12 +1,23 @@
 class ScraperDispatcher
   def self.scrape_commits
-    # TODO: set url to env variable = heroku host
-    conn = Faraday.new(url: 'http://4f4f870d.ngrok.io')
+    # refactor this to open a remote redis connection
     loop do
-      response = conn.get '/next_job'
-      puts JSON.parse(response.body)
-      gem_id = JSON.parse(response.body)['gem_id']
+      gem_id = next_gem_id
       GithubScraper.lib_commits(libraries: [ RubyGem.find(gem_id) ])
     end
+  end
+
+  private
+
+  def redis
+    # TODO: convert to environment variable
+    url = '104.236.81.65'
+    @redis ||= Redis.new(url: url)
+  end
+
+  def next_gem_id
+    next_id = redis.lpop('rubygems')
+    redis.rpush('rubygems', next_id)
+    next_id
   end
 end
