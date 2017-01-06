9091ba0a5ce3a7bd7a51eeb2c8a1787879eb3dac
Michael Mentele <Michaelrmentele@gmail.com>
2017-01-06 15:28:35 -0800

    added comments about scoring repos

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -2,4 +2,19 @@ class Repository < ActiveRecord::Base
   validates :github_id, uniqueness: true
 
   has_many :commits
+
+  # NOTE: Saved for later--when we begin scraping repos
+  # def self.update_score
+  #   avg_downloads = self.average(:downloads).to_i
+  #   avg_stars = self.average(:stars).to_i
+  #
+  #   # TODO: Make equation more mathematically correct (make it better)
+  #   star_multiplier = (avg_downloads / (avg_stars + 1)) + 200
+  #
+  #   self.all.each do |gem|
+  #     score = gem.downloads + gem.stars * star_multiplier
+  #     gem.update(score: score)
+  #   end
+  # end
+
 end
