1c84eb8ae68a0a53b4e1d204bd367435795d3f87
Michael Mentele <Michaelrmentele@gmail.com>
2017-01-06 15:17:23 -0800

    extracted redis ip to env var in figaro

--- lib/scraper/scraper_dispatcher.rb
+++ lib/scraper/scraper_dispatcher.rb
@@ -1,31 +1,29 @@
-# TODO: modify to reflect drop of ruby gems table
 class ScraperDispatcher
   def self.scrape_commits
-    # refactor this to open a remote redis connection
+    # TODO: refactor this to open a remote redis connection
     loop do
-      gem_id = next_gem_id
-      GithubScraper.lib_commits(libraries: [ RubyGem.find(gem_id) ])
+      repo_id = next_repo_id
+      GithubScraper.lib_commits(libraries: [ Repository.find(repo_id) ])
     end
   end
 
   def self.redis_requeue
     redis.flushall
-    RubyGem.all.each do |gem|
-      redis.rpush('rubygems', gem.id)
+    Repository.all.each do |repo|
+      redis.rpush('repositories', repo.id)
     end
   end
 
   private
 
   def self.redis
-    # TODO: convert to environment variable
-    ip = '104.236.81.65'
+    ip = ENV['REDIS_SERVER_IP']
     @redis ||= Redis.new(host: ip)
   end
 
-  def self.next_gem_id
-    next_id = redis.lpop('rubygems')
-    redis.rpush('rubygems', next_id)
+  def self.next_repo_id
+    next_id = redis.lpop('repositories')
+    redis.rpush('repositories', next_id)
     next_id
   end
 end
