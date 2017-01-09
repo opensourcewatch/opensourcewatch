ef58cb3749d2592d1cf2a3492910ce159c63e87b
Michael Mentele <Michaelrmentele@gmail.com>
2017-01-09 10:23:43 -0800

    updated activity score to include open issues

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -17,13 +17,13 @@ class Repository < ActiveRecord::Base
   private
 
   def activity_score
-    # TODO: we should only get the commits for a given time period, not all the commits on a repository
-    # TODO: Add migration and scraping for issues to the commit path as an option, then uncomment below
-    commits.count #+ open_issues.to_i
+    # TODO: We should only get the commits for a given time period, when calculating
+    # this, not all the commits on a repository
+    commits.count + open_issues.to_i
   end
 
   def significance_score
-    # to_i is to convert nil values to zero
+    # convert any nil values to zero with to_i
     stars.to_i + forks.to_i + watchers.to_i
   end
 end
