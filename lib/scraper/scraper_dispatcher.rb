dca0e3d1aa12a7e49329e883854c68e8f9cffbf7
Michael Mentele <Michaelrmentele@gmail.com>
2017-01-12 08:04:22 -0800

    combined similar methods so that the dispatcher can ask for the next repo of any given queue name and so that it only needs one handler for any given scraper.

--- lib/scraper/scraper_dispatcher.rb
+++ lib/scraper/scraper_dispatcher.rb
@@ -13,26 +13,25 @@ class ScraperDispatcher
 
   # TODO: Debug and test
   def self.scrape_metadata
-    repo_significance { GithubRepoScraper.update_repo_data([@current_repo]) }
+    scraper_handler { GithubRepoScraper.update_repo_data([@current_repo]) }
   end
 
   def self.scrape_commits
-    repo_activity { GithubRepoScraper.commits(repositories: [@current_repo]) }
+    scraper_handler { GithubRepoScraper.commits(repositories: [@current_repo]) }
   end
 
   def self.scrape_issues
-    repo_activity { GithubRepoScraper.issues(repositories: [@current_repo]) }
+    scraper_handler { GithubRepoScraper.issues(repositories: [@current_repo]) }
   end
 
   private
 
-  # TODO: refactor ? repo_activity and repo significance are almost identical
-  def self.repo_activity
+  def self.scraper_handler(queue_name)
     start_time = Time.now
     scrape_count = 0
 
     loop do
-      repo_data = JSON.parse(@redis_wrapper.next_active_repo)
+      repo_data = JSON.parse(@redis_wrapper.next_repo(queue_name))
 
       @current_repo = Repository.find(repo_data['id'])
 
@@ -42,23 +41,6 @@ class ScraperDispatcher
 
       scrape_count += 1
       puts "Scraped #{scrape_count} in #{((Time.now - start_time) / 60).round(2)} mins"
-    end
-  end
-
-  def self.repo_significance
-    start_time = Time.now
-    scrape_count = 0
-
-    loop do
-      repo_data = JSON.parse(@redis_wrapper.next_blank_repo)
-
-      @current_repo = Repository.find(repo_data['id'])
-
-      puts "Scraping: #{repo_data['url']}"
-      yield if block_given?
-      puts "Scraped #{scrape_count} in #{((Time.now - start_time) / 60).round(2)} mins"
-
-      scrape_count += 1
     end
   end
 end
