082a0bfeaf265fc3408d1fca7deb2f7f2c0f46cb
Michael Mentele <Michaelrmentele@gmail.com>
2017-01-12 11:03:24 -0800

    added to_json method, did some renaming and tweaked public interface for use with the priority queue

--- app/models/redis_repository.rb
+++ app/models/redis_repository.rb
@@ -2,27 +2,34 @@
 class RedisRepository
   include Comparable
 
-  attr_accessor :priority, :name
-  attr_reader :value, :times_popped
+  attr_reader :priority, :freq_popped, :priority_score
 
-  def initialize(id, name, value)
-    @id, @name, @value = id, name, value
+  def initialize(id, url, priority, freq_popped)
+    @id, @url, @priority, @freq_popped = id, url, priority, freq_popped
 
-    @freq_popped = 0
-
-    recalculate_priority
+    calculate_priority
   end
 
   def increment
     @freq_popped += 1
   end
 
-  def recalculate_priority
+  def calculate_priority
     # NOTE: If we just do this the values will continually decay toward 0
-    @priority_score = @value / (@times_popped + 1.0)
+    @priority_score = @priority / (@freq_popped + 1.0)
   end
 
   def <=>(other)
     @priority_score <=> other.priority_score
+  end
+
+  def to_json
+    {
+      id: @id,
+      url: @url,
+      priority: @priority,
+      priority_score: @priority_score,
+      freq_popped: @freq_popped
+    }.to_json
   end
 end
