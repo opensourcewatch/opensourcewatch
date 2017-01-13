844aea0413d310027b3084d861fbb42bae23b5b5
Lukas Nimmo <Lukas.nimmo@gmail.com>
2017-01-13 15:37:14 -0600

    Refactored file paths

--- daemon_commits.rb
+++ daemon_commits.rb
@@ -2,9 +2,6 @@ require 'fileutils'
 
 # Fix exceptions for 404 and 500
 
-FileUtils.mkpath './.daemon_tasks/tmp'
-FileUtils.mkpath './.daemon_tasks/tasks'
-
 # Won't work as it's the user of the RUNNING machine:
 # user = ENV['USER']
 
@@ -12,6 +9,9 @@ FileUtils.mkpath './.daemon_tasks/tasks'
 user = `echo $USER`.chomp
 
 working_directory = "/home/#{user}/workspace/capstone"
+
+pidfile_dir = FileUtils.mkpath("#{working_directory}/.daemon_tasks/tmp").first
+execution_dir = FileUtils.mkpath("#{working_directory}/.daemon_tasks/tasks").first
 
 command = ARGV[0]
 commands = ['start', 'stop', 'status']
@@ -22,11 +22,12 @@ processes = ['commits', 'issues', 'metadata']
 raise ArgumentError.new("Command must include #{processes}") unless processes.include?(process)
 execution_file = "#{process}_scraper"
 pid_file = "#{execution_file}.pid"
-pidfile_path = "#{working_directory}/.daemon_tasks/tmp/#{pid_file}"
-execution_path = "#{working_directory}/.daemon_tasks/tasks/#{execution_file}"
 
-# Set pid file
-File.new("/home/#{user}/workspace/capstone/.daemon_tasks/tmp/#{pid_file}", 'w+').close
+pidfile_path = "#{pidfile_dir}/#{pid_file}"
+execution_path = "#{execution_dir}/#{execution_file}"
+
+# Create pid file
+File.new("#{pidfile_path}", 'w+').close
 
 # Set rake task execution file
 task = case ARGV[1]
