42ba328139eee5fe61759c2b2c98d92b28d43458
Michael Mentele <Michaelrmentele@gmail.com>
2017-01-13 14:55:11 -0800

    added enqueing responsibility and options

--- lib/redis/priority_queue.rb
+++ lib/redis/priority_queue.rb
@@ -2,11 +2,11 @@
 class PriorityQueue
   PRIORITY_RANGE = (1..10).to_a
 
-  def initialize
-    @unique_queues = generate_queues
+  def initialize(enqueue: false, query: "stars > 10000", rescore: false)
+    @queue_base_name = ENV['REDIS_PRIORITY_QUEUE_BASE_NAME']
+    enqueue_redis(query, rescore) if enqueue
 
-    # add them to a skewed distribution list
-      # n! elements in queue
+    @unique_queues = generate_queues
     @queues = skewed_dist_of_queues(@unique_queues)
   end
 
@@ -18,15 +18,54 @@ class PriorityQueue
     queue.next
   end
 
+  def enqueue_redis(query, rescore)
+    tracked_repos = Repository.where(query)
+    puts "Retrieved #{tracked_repos.count} repositories"
+
+    puts "Scoring and ordering..."
+    calculate_scores(tracked_repos) if rescore
+    tracked_repos = tracked_repos.order(:score)
+
+    puts "Creating prioritized queues..."
+    enqueue_prioritized_sub_queues(tracked_repos)
+  end
+
   private
 
+  def enqueue_prioritized_sub_queues(tracked_repos)
+    bucket_size = (tracked_repos.count.to_f / PRIORITY_RANGE.length).ceil
+
+    # Enqueue a sub queue for each priority level
+    index = 0
+    tracked_repos.in_batches(of: bucket_size) do |batch|
+      priority = PRIORITY_RANGE[index]
+      queue_name = sub_queue_name(priority.to_s)
+      index += 1
+
+      sub_queue = CircularRedisQueue.new(batch, queue_name)
+      sub_queue.enqueue_redis
+    end
+  end
+
+  # For enqueing
+  def calculate_scores
+    count = 0
+    tracked_repos.in_batches do |batch|
+      batch.each do |repo|
+        repo.update_score
+        count += 1
+        puts "#{count} repos scored"
+      end
+    end
+  end
+
+  # For operation
   def generate_queues
     priority_tags = PRIORITY_RANGE
-    base_queue_name = ENV['REDIS_PRIORITY_QUEUE_BASE_NAME']
 
     unique_queues = []
     priority_tags.each do |tag|
-      queue_name = base_queue_name + '_' + tag.to_s
+      queue_name = sub_queue_name(tag.to_s)
       unique_queues << CircularRedisQueue.new(queue_name)
     end
     unique_queues
@@ -43,5 +82,9 @@ class PriorityQueue
       queues.pop
     end
     skewed_dist_of_queues
+  end
+
+  def sub_queue_name(extension)
+    @queue_base_name + "_" + extension
   end
 end
