4446ccff58217aca42ad787a1f32d66bc2cf827c
Michael Mentele <Michaelrmentele@gmail.com>
2017-01-13 11:43:48 -0800

    refactored api to be explicit and declaritive for each type of scraping activity

--- lib/scraper/scraper_dispatcher.rb
+++ lib/scraper/scraper_dispatcher.rb
@@ -8,32 +8,39 @@ class ScraperDispatcher
   @current_repo = nil
   @redis_wrapper = RedisWrapper.new
 
-  def self.scrape(queue_name: RedisWrapper::REDIS_ACTIVE_QUEUE_NAME, commits_on: true, issues_on: false)
-    if queue_name == RedisWrapper::REDIS_ACTIVE_QUEUE_NAME
-      scraper_handler(queue_name) do
-        GithubRepoScraper.commits(repositories: [@current_repo]) if commits_on
-        GithubRepoScraper.issues(repositories: [@current_repo]) if issues_on
-      end
-    elsif queue_name == RedisWrapper::REDIS_PRIORITY_QUEUE_NAME
-      priority_scraper_handler(queue_name)
-    elsif queue_name == RedisWrapper::REDIS_BLANK_QUEUE_NAME
-      # TODO: Debug and test
-      scraper_handler(queue_name) { GithubRepoScraper.update_repo_data([@current_repo]) }
-    else
-      raise "That queue name does not exist. Exiting Dispatcher."
+  # TODO: push knowledge of the env variables down to the individual queues and
+  # have three method calls
+
+  def self.scrape_repos(commits_on: true, issues_on: false)
+    queue = CircularRedisQueue.new(@redis_wrapper.redis)
+    scraper_handler(queue) do
+      GithubRepoScraper.commits(repositories: [@current_repo]) if commits_on
+      GithubRepoScraper.issues(repositories: [@current_repo]) if issues_on
     end
+  end
+
+  def self.scrape_prioritized_repos(commits_on: true, issues_on: false)
+    queue = PriorityQueue.new(@redis_wrapper.redis)
+    scraper_handler(queue) do
+      GithubRepoScraper.commits(repositories: [@current_repo]) if commits_on
+      GithubRepoScraper.issues(repositories: [@current_repo]) if issues_on
+    end
+  end
+
+  def self.scrape_blank_repos
+    # TODO: Debug and test
+    queue = RedisQueue.new(@redis_wrapper.redis, queue_name)
+    scraper_handler(queue_name) { GithubRepoScraper.update_repo_data([@current_repo]) }
   end
 
   private
 
-  def self.scraper_handler(queue_name)
+  def self.scraper_handler(queue)
     start_time = Time.now
     scrape_count = 0
 
     loop do
-      # TODO: dispatcher should work directly with a queue that uses redis underneath,
-      # not the other way around
-      repo_data = @redis_wrapper.next_repo(queue_name)
+      repo_data = queue.next
 
       @current_repo = Repository.find(repo_data['id'])
 
@@ -45,49 +52,4 @@ class ScraperDispatcher
       puts "Scraped #{scrape_count} in #{((Time.now - start_time) / 60).round(2)} mins"
     end
   end
-
-  def self.priority_handler
-    unique_queues = generate_queues
-
-    # add them to a skewed distribution list
-      # n! elements in queue
-    queues = skewed_dist_of_queues(unique_queues)
-
-    loop do
-      # get leftmost queue and push to the end
-      queue = queues.shift
-      queues.push(queue)
-
-      @current_repo = Repository.find(queue.next['id'])
-
-      GithubRepoScraper.commits(repositories: [@current_repo])
-    end
-  end
-
-  def self.generate_queues
-    priority_tags = RedisWrapper::PRIORITY_RANGE
-    base_queue_name = RedisWrapper::REDIS_PRIORITY_QUEUE_NAME
-
-    unique_queues = []
-    priority_tags.each do |tag|
-      queue_name = base_queue_name + '_' + tag.to_s
-      unique_queues << CircularRedisQueue.new(@redis_wrapper.redis, queue_name)
-    end
-    unique_queues
-  end
-
-  def self.skewed_dist_of_queues(queues)
-    # Want to create a distribution like:
-    #   [3, 2, 1] + [3, 2] + [3]
-    skewed_dist_of_queues = []
-    while queues.length > 0
-      queues.each do |queue|
-        skewed_dist_of_queues << queue
-      end
-      queues.pop
-    end
-    skewed_dist_of_queues
-  end
 end
-
-binding.pry
