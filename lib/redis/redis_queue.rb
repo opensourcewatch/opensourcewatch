eaa92f8fea33d166eaa593f32245c4734900274d
Michael Mentele <Michaelrmentele@gmail.com>
2017-01-14 12:31:25 -0800

    removed batches--seems to not work as expected, begins to pull records from the original pool even if the queury was limited; also clears redis queue before enqueuing

--- lib/redis/redis_queue.rb
+++ lib/redis/redis_queue.rb
@@ -10,21 +10,21 @@ class RedisQueue
   end
 
   def enqueue_redis
+    clear_queue
     # Insert into redis. The left side of the queue is the high side
     @redis.pipelined do
-      @repos.in_batches do |batch|
-        batch.each do |repo|
-          # Must be a string
-          member = {
-            name: repo.name,
-            id: repo.id,
-            url: repo.url,
-          }.to_json
+      @repos.each do |repo|
+        # Must be a string
+        member = {
+          name: repo.name,
+          id: repo.id,
+          url: repo.url,
+        }.to_json
 
-          # Use the priority value as the initial score
-          add(member)
-        end
-        puts "#{batch.count} repos enqueued."
+        # Use the priority value as the initial score
+        add(member)
+
+        puts "#{@repos.count} repos enqueued"
       end
     end
   end
@@ -35,6 +35,10 @@ class RedisQueue
   end
 
   private
+
+  def clear_queue
+    @redis.del(@queue_name)
+  end
 
   def add(member)
     @redis.rpush @queue_name, member
