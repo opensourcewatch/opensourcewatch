1731ca32a12b88d14972f7463c661e998198c90b
Michael Mentele <Michaelrmentele@gmail.com>
2017-01-19 14:49:41 -0800

    added matviews for issue activity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170119223031) do
+ActiveRecord::Schema.define(version: 20170119224135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
