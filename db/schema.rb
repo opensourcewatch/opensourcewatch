44137bdd2c42f94ac255652543172db1bc1e64c7
Michael Mentele <Michaelrmentele@gmail.com>
2017-01-19 14:33:39 -0800

    removed limit and selected all columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170119193736) do
+ActiveRecord::Schema.define(version: 20170119223031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
