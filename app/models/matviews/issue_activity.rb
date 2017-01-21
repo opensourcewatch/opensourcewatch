daad9ab91d8d183143bab865f47d86058b0f7f55
Michael Mentele <Michaelrmentele@gmail.com>
2017-01-21 15:54:46 -0800

    fixed autoload in rails by namespacing to match filenames

--- app/models/matviews/issue_activity.rb
+++ app/models/matviews/issue_activity.rb
@@ -1,18 +1,18 @@
-class Matviews::IssueActivity
-  class Matviews::IssueActivityLast0 < Matviews::Matview
+module Matviews::IssueActivity
+  class Last0 < Matviews::Matview
     self.table_name = "issue_activity_matview_last_0"
   end
 
-  class Matviews::IssueActivityLast7 < Matviews::Matview
+  class Last7 < Matviews::Matview
     self.table_name = "issue_activity_matview_last_7"
   end
 
-  class Matviews::IssueActivityLast30 < Matviews::Matview
+  class Last30 < Matviews::Matview
     self.table_name = "issue_activity_matview_last_30"
   end
 
 
-  class Matviews::IssueActivityLast90 < Matviews::Matview
+  class Last90 < Matviews::Matview
     self.table_name = "issue_activity_matview_last_90"
   end
 end
--- app/models/matviews/repo_activity.rb
+++ app/models/matviews/repo_activity.rb
@@ -1,18 +1,17 @@
-class Matviews::RepoActivity
-  class Matviews::RepoActivityLast0 < Matviews::Matview
+module Matviews::RepoActivity
+  class Last0 < Matviews::Matview
     self.table_name = "repo_activity_matview_last_0"
   end
 
-  class Matviews::RepoActivityLast7 < Matviews::Matview
+  class Last7 < Matviews::Matview
     self.table_name = "repo_activity_matview_last_7"
   end
 
-  class Matviews::RepoActivityLast30 < Matviews::Matview
+  class Last30 < Matviews::Matview
     self.table_name = "repo_activity_matview_last_30"
   end
 
-
-  class Matviews::RepoActivityLast90 < Matviews::Matview
+  class Last90 < Matviews::Matview
     self.table_name = "repo_activity_matview_last_90"
   end
 end
