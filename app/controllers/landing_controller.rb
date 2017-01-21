71a9f5e27783013f5658683b6f2da49d46379c18
Lukas Nimmo <Lukas.nimmo@gmail.com>
2017-01-21 17:34:12 -0600

    Refactored matview retrieval into a method.

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,9 +1,31 @@
 class LandingController < ApplicationController
   def index
     Rails.application.eager_load!
-    @repos = Matviews::RepoActivityLast0.limit(10)
-    @committers = Matviews::TopUserLast0.limit(10)
-    @issues = Matviews::IssueActivityLast0.limit(10)
-    @chatty = Matviews::ChattiestUserLast0.limit(10)
+    set_activity_vars
+  end
+
+  private
+
+  def date_range_postfix
+    case params['date_range']
+    when 'Today'
+      'Last0'
+    when 'Weekly'
+      'Last7'
+    when 'Monthly'
+      'Last30'
+    when 'Last 90'
+      'Last90'
+    else
+      'Last0'
+    end
+  end
+
+  def set_activity_vars
+    postfix = date_range_postfix
+    @repos = Module.const_get("Matviews::RepoActivity#{postfix}").limit(10)
+    @committers = Module.const_get("Matviews::TopUser#{postfix}").limit(10)
+    @issues = Module.const_get("Matviews::IssueActivity#{postfix}").limit(10)
+    @chatty = Module.const_get("Matviews::ChattiestUser#{postfix}").limit(10)
   end
 end
