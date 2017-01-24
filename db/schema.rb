ec1ecd8ace5a1605b809a79b7ae6d451d24ebae2
Lukas Nimmo <Lukas.nimmo@gmail.com>
2017-01-24 01:18:53 -0600

    Recreated issue matviews after cascade from removing open_date col from issue (which matviews depended on)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124053242) do
+ActiveRecord::Schema.define(version: 20170124071644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
