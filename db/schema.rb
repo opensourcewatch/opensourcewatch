7f8226617705aeb4ea0fdd6f8fd5a387f0cce54b
Lukas Nimmo <Lukas.nimmo@gmail.com>
2017-02-01 01:35:01 -0600

    Removed n+1 query from landing page for issue comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125062652) do
+ActiveRecord::Schema.define(version: 20170201070232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
